1a83976a235099bd7dc1ed3c969bed3e5813a90f
Ishara Rathnayake <ishara11rathnayake@gmail.com>
2025-04-06 16:20:19 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavBar from '@/components/Navbar.vue'
+import Hero from '@/components/HeroSection.vue'
+import HomeCards from '@/components/HomeCards.vue'
+</script>
+
+<template>
+  <NavBar />
+  <Hero />
+  <HomeCards />
+</template>
