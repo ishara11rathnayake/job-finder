3c05a6a401dbd7bc10e4005b3234567c5bb290af
Ishara Rathnayake <ishara11rathnayake@gmail.com>
2025-04-14 23:43:38 +1000

    add job-listing component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import NavBar from '@/components/Navbar.vue'
+import NavBar from '@/components/NavBar.vue'
 import Hero from '@/components/HeroSection.vue'
 import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
 </script>
 
 <template>
   <NavBar />
   <Hero />
   <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
